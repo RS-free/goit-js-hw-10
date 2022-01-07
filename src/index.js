f8707b0d837ab0ee694954ffc1b7692adc84e410
Ruslan Sulieimanov <ruslansulieim@gmail.com>
2022-01-07 11:41:02 +0200

    add

--- src/index.js
+++ src/index.js
@@ -46,3 +46,26 @@ function onSearch(elem) {
       return error;
     });
 }
+
+function countryId({ flags, name }) {
+  return `
+    <li class = country-item>
+    <img class = 'country-list__flags' src="${flags.svg}" alt="${name.official}" width=100/>
+    <h2 class = country-list__name>${name.official}</h2>
+    </li>`;
+}
+
+function countryData({ name, capital, population, flags, languages }) {
+  return `
+    <div class='country-info'>
+        <h2 class='country-title'>${name.official}</h2>
+        <p class='country-text'>Capital: <span class='country-subtext'>${capital}</span></p>
+        <p class='country-text'>Population: <span class='country-subtext'>${population}</span></p>
+        <p class='country-text'>Flags: <img class='country-flag' src='${flags.svg}' alt='${
+    name.official
+  }' width=50/></p> 
+        <p class='country-text'>Languages: <span class='country-subtext'>${Object.values(
+          languages,
+        )}</span></p>
+    </div>`;
+}
